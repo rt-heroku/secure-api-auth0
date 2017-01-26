b310a0c0181f7e34904a2c214dafe30c5a2dcd81
rt-heroku <rtorres@heroku.com>
2017-01-26 09:24:37 -0500

    init

---
+++ src/main/java/com/heroku/SecureApiApplication.java
@@ -0,0 +1,12 @@
+package com.heroku;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecureApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecureApiApplication.class, args);
+	}
+}
