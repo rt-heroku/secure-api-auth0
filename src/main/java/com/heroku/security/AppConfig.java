44b515dc70134ab3ae0fc7eb9fec62a65a5f0bcd
rt-heroku <rtorres@heroku.com>
2017-01-26 17:45:04 -0500

    more

--- src/main/java/com/heroku/security/AppConfig.java
+++ src/main/java/com/heroku/security/AppConfig.java
@@ -4,7 +4,6 @@ import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
