4363bb54eabd4257173bec2251765f06f08bb630
rt-heroku <rtorres@heroku.com>
2017-01-27 12:52:38 -0500

    more

--- src/main/java/com/heroku/security/CORSFilter.java
+++ src/main/java/com/heroku/security/CORSFilter.java
@@ -1,14 +1,7 @@
 package com.heroku.security;
 
-import org.springframework.stereotype.Component;
-
-import javax.servlet.*;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-
-@Component
-public class CORSFilter implements Filter {
+//@Component
+public class CORSFilter{ /* implements Filter {
 
     @Override
     public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
@@ -33,4 +26,5 @@ public class CORSFilter implements Filter {
     public void destroy() {
 
     }
+    */
 }
