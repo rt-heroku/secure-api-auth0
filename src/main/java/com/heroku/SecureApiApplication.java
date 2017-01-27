29c433a7937d1ae87c56244f22cb28d98521cdf2
rt-heroku <rtorres@heroku.com>
2017-01-27 12:32:20 -0500

    more

--- src/main/java/com/heroku/SecureApiApplication.java
+++ src/main/java/com/heroku/SecureApiApplication.java
@@ -10,9 +10,10 @@ import org.springframework.context.annotation.PropertySources;
 @SpringBootApplication
 @ComponentScan(basePackages = {"com.heroku", "com.auth0.spring.security.api"})
 @EnableAutoConfiguration
-@PropertySources({
-		@PropertySource("classpath:auth0.properties")
-})public class SecureApiApplication {
+//@PropertySources({
+//		@PropertySource("classpath:auth0.properties")
+//})
+public class SecureApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SecureApiApplication.class, args);
